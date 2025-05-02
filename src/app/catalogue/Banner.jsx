697805a62094a9325c1904d8a42109691f56bd88
MauricioYeverino2003 <maukoyi@hotmail.com>
2025-05-02 16:14:12 -0700

    About to finish

--- src/app/catalogue/Banner.jsx
+++ src/app/catalogue/Banner.jsx
@@ -6,9 +6,10 @@ import { getCatalogueById } from '../lib/firestoreHelpers';
 
 function Banner() {
   const [catalogue, setCatalogue] = useState([]);
+  const [bannerUrl, setBannerUrl] = useState("https://firebasestorage.googleapis.com/v0/b/windowshop-cd3d9.firebasestorage.app/o/Banner%2F9f5ad391-3a8e-4b44-94a5-c108ecf78f39-background.png?alt=media&token=d597843e-e0c5-45d0-829b-c3f3e0ca6142")
     const params = useParams();
     const catalogueId  = params.catalogueId;
-    
+    // banner url
     useEffect(() => {
       if (!catalogueId) {
         alert("Catalogue ID not found");
@@ -19,6 +20,7 @@ function Banner() {
         try {
           const fetched = await getCatalogueById(catalogueId);
           console.log(fetched[0])
+          setBannerUrl(fetched[0].bannerImage)
           setCatalogue(fetched[0])
         } catch (err) {
           console.log("Failed loading products:", err);
@@ -30,7 +32,7 @@ function Banner() {
     <>
       <div className="relative w-full h-60">
         <Image
-          src="/images/background.png"
+          src={bannerUrl}
           alt="Shop banner"
           layout="fill"
           objectFit="cover"
